ec069cb35679629717e2914adf367884f65f2c82
Carlos López <cladev@gmail.com>
2017-06-11 17:15:50 +0200

    Avoid dependency with internet connection

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,7 +8,7 @@
     <title>Ticket's equations</title>
 
 	<!-- Bootstrap core CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="{{asset("css/bootstrap.min.css")}}">
 
     <!-- Custom styles for this template -->
 	<style>
@@ -65,7 +65,7 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="{{asset("js/jquery.min.js")}}"></script>
+	<script src="{{asset("js/bootstrap.min.js")}}"></script>
   </body>
 </html>
